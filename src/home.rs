a7d7796102ad3beb9928421b570e7922e5e08cd7
hronro <hronro@gmail.com>
2026-06-01 19:14:11 +0800

    :recycle: drop unneeded Result return from home::get

--- src/home.rs
+++ src/home.rs
@@ -2,17 +2,20 @@ use axum::response::Html;
 use sailfish::TemplateOnce;
 
 use crate::auth::Claims;
-use crate::errors::ServerError;
 use crate::files;
 use crate::templates::Home;
 
 pub const ROUTE_PATH: &str = "/";
 
-pub async fn get(claims: Option<Claims>) -> Result<Html<String>, ServerError> {
+pub async fn get(claims: Option<Claims>) -> Html<String> {
     let home_template = Home {
         claims,
         jump_url: files::ROUTE_PATH_ROOT,
     };
 
-    Ok(Html(home_template.render_once()?))
+    Html(
+        home_template
+            .render_once()
+            .expect("home template render is infallible with default String buffer"),
+    )
 }
